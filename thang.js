21fd978e8074f40c44cf990227451506299a4f1e
thang551999 <duongphucthangts@gmail.com>
2020-09-23 22:16:29 +0700

    thangthuansontest

---
+++ thang.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class thang extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ thangthuanson.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class thangthuanson extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
